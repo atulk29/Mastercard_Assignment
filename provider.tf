fa51a37e2a2f99e184602e38cc0ee49b85411981
atulk29 <48256118+atulk29@users.noreply.github.com>
2022-06-03 11:02:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws"{
   region = "ap-south-1"
-  access_key = "AKIAY22D6ITUUJLFGPVZ"
-  secret_key = "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"
+  access_key = "xxxxxxxxxxxxxxxxxxxxxxx"
+  secret_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   version = "~> 3.0"
 }
